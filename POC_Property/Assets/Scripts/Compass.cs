2ffe1daadd0557ab02ca3d02984490a31ff55193
Arthur <a.spellani@rubika-edu.com>
2021-05-17 16:58:37 +0200

    RealeaseDay_1

---
+++ POC_Property/Assets/Scripts/Compass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public GameObject rotateObj;
+
+    private void Update()
+    {
+        transform.rotation = rotateObj.transform.rotation;
+    }
+}
